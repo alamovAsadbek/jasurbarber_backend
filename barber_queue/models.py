b01b0431301076db881f88e2dc4ac4289d144264
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2025-10-30 20:54:57 +0500

    changed

--- barber_queue/models.py
+++ barber_queue/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 
+# Navbat
 class Queue(models.Model):
     customer_first_name = models.CharField(max_length=100)
     customer_last_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
-    service = models.CharField(max_length=100)
-    total = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    service = models.CharField(max_length=100, null=True, blank=True)
+    total = models.DecimalField(max_digits=12, decimal_places=2, default=0, null=True, blank=True)
     date = models.DateTimeField()  # foydalanuvchi kiritadigan sana + soat
     whom = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
--- barber_queue/views.py
+++ barber_queue/views.py
@@ -1,14 +1,17 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.utils.timezone import now
 from datetime import timedelta
 from .models import Queue
 from .serializers import QueueSerializer
 
+
 class QueueViewSet(viewsets.ModelViewSet):
     queryset = Queue.objects.all().order_by('-date')
     serializer_class = QueueSerializer
+    permission_classes = (AllowAny,)
 
     @action(detail=False, methods=['get'])
     def today(self, request):
